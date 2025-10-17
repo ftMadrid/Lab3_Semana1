23572420f0d557eb3b5a1e801590fa85b60d0236
ftMadrid <fernandomadridcv04@gmail.com>
2025-10-17 10:07:39 -0600

    FInished clas Vehicle.cpp

--- Semana1/vehicle.cpp
+++ Semana1/vehicle.cpp
@@ -1,7 +1,23 @@
 #include "vehicle.h"
+#include <iostream>
 
-
-Vehicle::Vehicle()
-{
-
+Vehicle::Vehicle(string name_){
+    this->name_ = name_;
+    this->fuelLevel_ = 100;
 }
+
+void Vehicle::normalizeFuelLevel(){
+    fuelLevel_ = max(0, fuelLevel_);
+    fuelLevel_ = min(100, fuelLevel_);
+}
+
+string Vehicle::getName(){
+    return this->name_;
+}
+
+short Vehicle::getFuelLevel(){
+    return this->fuelLevel_;
+}
+
+
+
--- Semana1/vehicle.h
+++ Semana1/vehicle.h
@@ -16,25 +16,13 @@ protected:
 
 public:
 
-    void normalizeFuelLevel(){
+    void normalizeFuelLevel();
 
-        fuelLevel_ = max(0, fuelLevel_);
-        fuelLevel_ = min(100, fuelLevel_);
+    Vehicle(string name_);
 
-    }
+    string getName();
 
-    Vehicle(const string& name_){
-        this->name_ = name_;
-        this->fuelLevel_ = 100;
-    }
-
-    string getName(){
-        return name_;
-    }
-
-    ushort getFuelLevel(){
-        return fuelLevel;
-    }
+    short getFuelLevel();
 
     virtual short refuel(short units) = 0;
 
