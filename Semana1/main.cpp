497fa39f862284c6bc561cc0b3f84212ac3f156d
ftMadrid <fernandomadridcv04@gmail.com>
2025-10-17 08:25:29 -0600

    Add hello world (testing git)

--- Semana1/main.cpp
+++ Semana1/main.cpp
@@ -1,7 +1,15 @@
 #include <QCoreApplication>
+#include <iostream>
+
+using std::cout;
+
+#define print cout <<
 
 int main(int argc, char *argv[]) {
 
     QCoreApplication a(argc, argv);
+
+    print "Hello World!";
+
     return a.exec();
 }
